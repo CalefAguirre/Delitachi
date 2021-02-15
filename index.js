736e10dbb809d6599d8bf27bf06102580132756d
Calef Aguirre <aguirrecalef@gmail.com>
2021-02-14 23:21:52 -0300

    Now we're heading somewhere

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const express  = require("express");
+const http = require('http');
+const express = require("express");
+//const mongoose = require("mongoose");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.end("works!");
+});
+
+app.listen(3000, () => {
+  console.log("server initiated");
+});
