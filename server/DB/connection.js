beb53fc7dc29b4e178bdd8b50315043c50752327
calef aguirre <aguirrecalef@gmail.com>
2021-02-25 19:32:40 -0300

    again, issues

--- server/DB/connection.js
+++ server/DB/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = 'mongodb+srv://AguirreCalef:4bGtXg4tGIZEGjB4@cluster0.3v3xb.mongodb.net/Delitachi?retryWrites=true&w=majority';
+const uri = require('./server/DB/uri');
 
 const connectDB = async () => {
   await mongoose.connect(uri, {
